752d8c994d6695f9cf23cc364aa153cf18f45036
Shany Biton <shany.biton@ibm.com>
2026-02-11 14:04:26 +0200

    omit data preparation links and info

--- bmfm_targets/datasets/scp1884/scp1884_dataset.py
+++ bmfm_targets/datasets/scp1884/scp1884_dataset.py
@@ -17,19 +17,12 @@ class SCP1884Dataset(BaseRNAExpressionDataset):
     A PyTorch Dataset for scp1884 h5ad.
     https://singlecell.broadinstitute.org/single_cell/study/SCP1884/human-cd-atlas-study-between-colon-and-terminal-ileum.
 
-    h5ad is generated downloaded tree from above URL using this notebook.
-    https://github.ibm.com/A1KOSUGI/gsetests/blob/main/scp1884.ipynb
-
     Attributes
     ----------
             data_dir (str | Path): Path to the directory containing the data.
             split (str): Split to use. Must be one of train, dev, test.
             split_params (dict, optional): _description_. Defaults to default_split.
             transforms (list[dict] | None, optional): List of transforms to be applied to the datasets.
-    Dataset Description
-    ----------
-            We obtained full dataset from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE156728, and converted using functions written in gse156729_util.py
-            Notebooks to generate pre-processed files using gse156729_util.py are stored in the separate repository.
 
     """
 
